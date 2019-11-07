a5298dc9ca9fd4766452bc9d3a0eebcd80183d87
Tierbolg <tierbolg@outlook.com>
2019-11-07 11:09:49 -0600

    refactor and working

--- ven00m/ven00mScraper.py
+++ ven00m/ven00mScraper.py
@@ -9,34 +9,39 @@ from bs4 import BeautifulSoup
 import PROPERTIES
 
 
+def obtainparsefromhtml(parsingUrl):
+    """Return the information of html parsed"""
+    datos = urllib.request.urlopen(parsingUrl).read().decode()
+    return BeautifulSoup(datos)
+
+
 def main():
     """Central process"""
-    #Get the name of the album
-    albumName=workingwithtitle(PROPERTIES.DOWNLOAD_URL)
+    # Get common object to parse the html
+    soup = obtainparsefromhtml(PROPERTIES.DOWNLOAD_URL)
+    # Get the name of the album
+    albumName = soup.findAll("span", {"class": "showalbumheader__gallerytitle"})[
+        0].get_text()
+    # Get the description of the album
+    albumDescription = soup.findAll(
+        "div", {"class": "showalbumheader__gallerysubtitle"})[0].get_text()
     print("The name of the album is: "+albumName)
-    #Get the list of images by album
-    listImagesFiltered=workingwithalbumimages(PROPERTIES.DOWNLOAD_URL)
+    print("The description of the album is: "+albumDescription)
+    # Get the list of images by album
+    listImagesFiltered = workingwithalbumimages(soup)
     print("List of images by album: "+listImagesFiltered)
 
-def workingwithtitle(parsingUrl):
-    nameOfAlbum="tristania"
-    datos2 = urllib.request.urlopen(parsingUrl).read().decode()
-    soup = BeautifulSoup(datos2)
-    return soup.findAll("span",{"class":"showalbumheader__gallerytitle"})[0].get_text()
 
-
-def workingwithalbumimages(parsingUrl):
+def workingwithalbumimages(soupObtained):
     """Return the information for storage therow of each album"""
-    datos = urllib.request.urlopen(parsingUrl).read().decode()
-    soup = BeautifulSoup(datos)
-    tags = soup('img')
+    tags = soupObtained('img')
     listOfImages = []
     for tag in tags:
         srcToCheck = tag.get('src')
         if not (srcToCheck == None) and ("small" in srcToCheck):
-            #print(srcToCheck)
+            # print(srcToCheck)
             listOfImages.append(PROPERTIES.HTTPS_CONSTANT+srcToCheck)
-    str1 = ", "    
+    str1 = ", "
     #print("final: "+str1.join(listOfImages))
     return str1.join(listOfImages)
 
